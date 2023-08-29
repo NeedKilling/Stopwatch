e29d6fcd5d2d919449684be8c0e8a9dfebe9baba
Makar <vsyakofazno@gmail.com>
2023-08-29 22:16:42 +0300

    realization stopwatch used Date()

--- src/js/main.js
+++ src/js/main.js
@@ -8,39 +8,43 @@ const miliSeconds = document.getElementById('miliSeconds');
 const play = document.querySelector('#play')
 
 
-let miliSecondsValue = 0;
-let secondsValue = 0;
-let minetsValue = 0;
+let startTime;
 let time;
 
+let minet;
+let second;
+let miliSecond;
+let nowTime = 0;
+
 function timeCounter(){
-    miliSecondsValue++
-    miliSeconds.innerHTML = miliSecondsValue < 10 ? "0" + miliSecondsValue : miliSecondsValue;
-    if(miliSecondsValue > 99){
-        miliSecondsValue = 0
-        secondsValue++
-        seconds.innerHTML = secondsValue < 10 ? "0" + secondsValue : secondsValue;
-    }
-    if(secondsValue > 59){
-        secondsValue = 0
-        minetsValue++
-        minets.innerHTML = minetsValue < 10 ? "0" + minetsValue : minetsValue;
-    }
+    let curentTime = new Date().getTime();  // текущее время 
+    nowTime = curentTime - startTime;   // разница во времени в милисикундах
+    console.log(nowTime)
+    
+    let totalsecond = Math.floor(nowTime/1000);
+
+    minet = Math.floor(totalsecond/60);
+    second = totalsecond % 60
+    miliSecond = nowTime % 1000;
+    
+    minets.innerHTML = minet < 10 ? "0" + minet : minet
+    seconds.innerHTML = second < 10 ? "0" + second : second
+    miliSeconds.innerHTML = miliSecond.toString().slice(0,-1)
 }
 
 buttons.addEventListener('click',(e)=>{
     switch(e.target.id){
         case "play":
-            clearInterval(time)
+            startTime = new Date().getTime()   // новое время как точка отсчета
             time = setInterval(timeCounter,10)
             break;
         case "stop":
             clearInterval(time)
             break;
-        case "reset":  
-            miliSecondsValue = 0;
-            secondsValue = 0;
-            minetsValue = 0;
+        case "reset": 
+            minet = 0;
+            second = 0;
+            miliSecond = 0;
             miliSeconds.innerHTML = "00"
             seconds.innerHTML = "00"
             minets.innerHTML = "00"
